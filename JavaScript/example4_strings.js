b951d61adedf01fa6796cedd1bdf866b0fe071e9
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-01-22 11:45:42 -0500

    Added skeletons for HW1

--- JavaScript/example4_strings.js
+++ JavaScript/example4_strings.js
@@ -1,4 +1,6 @@
 let name = "Mark";
-console.log(`Hello ${1}`); 			// ?
-console.log(`Hello ${"name"}`);		// ?
-console.log(`Hello ${name}`);		// ?
+console.log(`Hello ${1}`);       // ?
+console.log(`Hello ${"name"}`);  // ?
+console.log(`Hello ${name}`);    // ?
+console.log(`Hello ${name + "a"}`);    // ?
+console.log("Hello ${name}");    // ?
