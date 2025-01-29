f076939c641a7df07ff980f992b381d80a2b8858
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-01-29 15:27:58 -0500

    demo 2 - L5

--- JavaScript/live_arrays.js
+++ JavaScript/live_arrays.js
@@ -14,50 +14,53 @@ let output1 = isBelowThreshold(array1);
 console.log(`DEMO1 (imperative): ${output1}`);	// true
 
 
-// DEMO 1: Array.every(callback[,thisArg]) 
-let outputDemo1 = /* ... TODO ... */;
+// // DEMO 1: Array.every(callback[,thisArg]) 
+let callback1 = (n) => n <= 40;
+let outputDemo1 = array1.every(callback1);
 console.log(`DEMO1 (declarative): ${outputDemo1}`);
 
 
-// // -------------------------------------------------------------------------
-// // EXAMPLE 2: imperative-style
-// // return an array of all words whose size are higher than 6.
-// // -------------------------------------------------------------------------
-// function getAllWords(words){
-// 	let result = [];
-// 	for(let i = 0; i < words.length ; i++){
-// 		if(words[i].length > 6)
-// 			result.push(words[i]);
-// 	}
-// 	return result;
-// }
-// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
-// let output2 = getAllWords(words); // [exuberant,destruction,present]
-// console.log(`DEMO2 (imperative): ${output2}`) 
+// -------------------------------------------------------------------------
+// EXAMPLE 2: imperative-style
+// return an array of all words whose size are higher than 6.
+// -------------------------------------------------------------------------
+function getAllWords(words){
+	let result = [];
+	for(let i = 0; i < words.length ; i++){
+		if(words[i].length > 6)
+			result.push(words[i]);
+	}
+	return result;
+}
+const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+let output2 = getAllWords(words); // [exuberant,destruction,present]
+console.log(`DEMO2 (imperative): ${output2}`) 
 
 // // DEMO2: Array.filter(callback[,thisArg]) 
-// let outputDemo2 = /* ... TODO ... */;
-// console.log(`DEMO2 (declarative):${outputDemo2}`);
+// let callback2 = (word) => (word.length > 6);
+let outputDemo2 = words.filter((word) => (word.length > 6));
+console.log(`DEMO2 (declarative):${outputDemo2}`);
 
 
-// // -------------------------------------------------------------------------
-// // EXAMPLE 3: imperative-style
-// // Returns a new array in which each element is multiplied by 2
-// // -------------------------------------------------------------------------
-// function multiplyByTwo(array){
-// 	let result = [];
-// 	for(let i = 0; i < array.length ; i++){
-// 		result.push(array[i] * 2);
-// 	}
-// 	return result;
-// }
-// const demo3Arr = [1, 4, 9, 16];
-// let output3 = multiplyByTwo(demo3Arr);
-// console.log(`DEMO3 (imperative): ${output3}`); // [2, 8, 18, 32]
+// -------------------------------------------------------------------------
+// EXAMPLE 3: imperative-style
+// Returns a new array in which each element is multiplied by 2
+// -------------------------------------------------------------------------
+function multiplyByTwo(array){
+	let result = [];
+	for(let i = 0; i < array.length ; i++){
+		result.push(array[i] * 2);
+	}
+	return result;
+}
+const demo3Arr = [1, 4, 9, 16];
+let output3 = multiplyByTwo(demo3Arr);
+console.log(`DEMO3 (imperative): ${output3}`); // [2, 8, 18, 32]
 
-// // DEMO3: Array.map(callback[,thisArg])
-// let outputDemo3 = /* ... TODO */;
-// console.log(`DEMO3 (declarative): ${outputDemo3}`);
+// DEMO3: Array.map(callback[,thisArg])
+let callback3 = (n) => n *2;
+let outputDemo3 = demo3Arr.map(callback3);
+console.log(`DEMO3 (declarative): ${outputDemo3}`);
 
 // // -------------------------------------------------------------------------
 // // EXAMPLE 4: imperative-style
