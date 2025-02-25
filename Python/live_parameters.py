717561761e2d7eb6c4858f12d7edc033cba6ea53
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-02-25 15:17:18 -0500

    Added python scripts demo-ed in class

--- Python/live_parameters.py
+++ Python/live_parameters.py
@@ -1,4 +1,4 @@
-def f(x, y, z):
+def f(x, y, z=12):
     print(f'x={x} y={y} z={z}')
 
 
@@ -6,7 +6,7 @@ def f(x, y, z):
 # f(0,1)    			# how about this?
 f(0, 1, 2)  # and now?
 f(y=11, x=10, z=12)  # keyword arguments
-f(
-    10, z=12, y=11
-)  # mixing keyword arguments and positional arguments (pos. args shoulld come before kw args)
-# f(x=10, 11, 12) this wont work #sad
+# f(
+#     z=12, y=11, 10
+# )  # mixing keyword arguments and positional arguments (pos. args shoulld come before kw args)
+f(x=10, 11, 12) # this wont work #sad
--- Python/live_parameters2.py
+++ Python/live_parameters2.py
@@ -1,4 +1,3 @@
-
 def write_rule(selector, *comments, **options):
     for c in comments:
         print(f"/* {c} */")
@@ -9,8 +8,7 @@ def write_rule(selector, *comments, **options):
     print('}')
 
 
-write_rule('h1', 'Change all heading 1s', 'Font styles', font_family='Helvetica', size='20px')
-write_rule('p.error', color='red', margin='16px', padding='0')
-
+# write_rule('h1', 'Change all heading 1s', 'Font styles', font_family='Helvetica', size='20px')
+# write_rule('p.error', color='red', margin='16px', padding='0')
 # write_rule('p', comments = 'something', "Joanna") # not work!
 write_rule("p")
