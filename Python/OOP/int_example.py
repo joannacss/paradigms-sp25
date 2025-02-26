5afe0c939351aa5ed233be5d385e412cddf95f8a
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-02-26 15:08:36 -0500

    code demoed in class

--- Python/OOP/int_example.py
+++ Python/OOP/int_example.py
@@ -1,7 +1,7 @@
 class int:
     def __init__(self, x):
         self.x = x
-
+    # obj1 + obj2
     def __add__(self, obj):
         return self.x - obj.x
 
--- Python/OOP/vector.py
+++ Python/OOP/vector.py
@@ -6,12 +6,24 @@ class Vector:
         self.y = y
 
     #TODO: implement methods
+    def magnitude(self):
+        return math.sqrt(self.x*self.x + self.y*self.y)
 
-# u = Vector(3,4)
-# v = Vector(-5,10)
-#
-# print(u.x) # 3
-# print(u.y) # 4
-# print(u.magnitude()) # 5.0
-# print(u == v) # <-2,14>
-# print(u * v) # 25
+    def __add__(self, obj):
+        return Vector(self.x + obj.x, self.y + obj.y)
+    # obj1 * obj2
+    def __mul__(self, other):
+        return self.x*other.x + self.y*other.y
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+
+
+
+u = Vector(3,4)
+v = Vector(-5,10)
+
+print(u.x) # 3
+print(u.y) # 4
+print(u.magnitude()) # 5.0
+print(u + v) # <-2,14>
+print(u * v) # 25
