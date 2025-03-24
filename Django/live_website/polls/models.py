13c96681f9a197f4e4c9b4cf9850aff6cd57aa02
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-03-24 14:28:21 -0400

    Live demo 1

--- Django/live_website/polls/models.py
+++ Django/live_website/polls/models.py
@@ -1,16 +1,21 @@
 from django.db import models
 
+
 # Create your models here.
 
 
 class Question(models.Model):
     question_text = models.CharField(max_length=500)
     pub_date = models.DateTimeField()
+
     def __str__(self):
         return self.question_text
+
+
 class Choice(models.Model):
-    question_id =  models.ForeignKey(Question, on_delete=models.CASCADE)
+    question_id = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=500)
     votes = models.IntegerField()
 
-
+    def __str__(self):
+        return f"{self.choice_text} - (#Votes = {self.votes})"
