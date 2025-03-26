77befba3700143f17d76d4feabc3e8929e8949f2
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-03-26 14:40:14 -0400

    Added live demo-ed code

---
+++ REST/live_rest_client.py
@@ -0,0 +1,22 @@
+import requests
+import csv
+HOST = "http://jcssdev.pythonanywhere.com/"
+
+
+with open("bugs.csv", "w") as f:
+	next_page = HOST + "bugs"
+	while next_page:
+		csv_writer = csv.writer(f, delimiter=",", quotechar="'")
+		response = requests.get(next_page)
+
+		json_resp = response.json()
+		csv_writer.writerow(["bug_id", "status", "package", "summary"])
+
+		for bug in json_resp["results"]:
+			bug_id = bug["id"]
+			status = bug["status"]
+			package = bug["package"]
+			summary = bug["summary"]
+			csv_writer.writerow([ bug_id, status, package, summary])
+
+		next_page = json_resp["next"]
