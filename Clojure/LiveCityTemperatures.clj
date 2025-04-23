8018bb8873e4b57662753e6607ff872c027a3957
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-04-23 14:39:05 -0400

    Demo 1'

--- Clojure/LiveCityTemperatures.clj
+++ Clojure/LiveCityTemperatures.clj
@@ -14,33 +14,18 @@
 ; map from a list of rows to:
 ; 				{Los Angeles (70.5), Honolulu (80.3), South Bend (55.8), Los Angeles (71.2) ...}
 ;;; first declare callback funtion `parse-row` that maps each row to {"city" (temperature)}
-(defn parse-row [r] (let [column (str/split r #",")
-																										city (get column 0)
-																										temperature (Float/parseFloat (get column 1))
-																									] 
-																									{city (list temperature)}))
+
+
 ;;; apply the callback function to each element in the `rows` list 
-(def rows (map parse-row rows))
+
 
 
 ; use the `reduce` function to merge temperatures by city to create a map like this:
 ; {"Los Angeles" (70.5 71.2 ...), "Honolulu" (80.3 81.0 ...), "South Bend" (55.8 54.5 ...)}
 ; store it on a variable `temperatures_map`
 ;;;; map-merger: function that merge two maps
-(defn map-merger [x y] (concat (or x '()) y))
-
 ;;;; city-reducer: callback that takes `e1` `e2` and updates `e1` with what is in `e2`
-(defn city-reducer [e1 e2] (let [e2_city (key (first e2))
-																																	e2_temp (val (first e2))]
-																																	(update e1 e2_city map-merger e2_temp)
-																											 )
-)
-
-
 (def temperatures_map (reduce city-reducer rows))
-
-
-
 
 ; Average function
 (defn avg [p] (double (/ (reduce + p)  (count p))))
--- Clojure/LiveTemperatures.clj
+++ Clojure/LiveTemperatures.clj
@@ -5,12 +5,19 @@
 (def filepath "./data/temperatures.csv")
 
 ; parse into rows using slup and str/split by new lines #"\n"
-
+(def rows (str/split (slurp filepath) #"\n"))
 
 ; discard header using the `rest` function
+(def rows (rest rows))
+
 
 
 ; use `(map callback collection)` to make it a collection of floating point numbers
 
+(def rows (map (fn [x] (Float/parseFloat x)) rows))
 ; print metrics
+(defn avg [c] (/ (reduce + c) (count c)))
 
+(println (apply min rows))
+(println (apply max rows))
+(println (avg rows))
