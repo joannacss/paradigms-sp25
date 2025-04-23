eb9dfa6e8163c28f36598b2bf33bdf8f16e4107e
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2025-04-23 14:47:08 -0400

    Demo 1'

--- Clojure/LiveCityTemperatures.clj
+++ Clojure/LiveCityTemperatures.clj
@@ -17,23 +17,29 @@
 
 
 ;;; apply the callback function to each element in the `rows` list 
-
-
-
+(def parse-row [row] (let
+																								[column (str/split row #",")
+																									city (first column)
+																									temperature (Float/parseFloat (nth column 1))]
+																									{city (list temperature)}
+																						)
+)
+(def rows (map parse-row rows))
+(println rows)
 ; use the `reduce` function to merge temperatures by city to create a map like this:
 ; {"Los Angeles" (70.5 71.2 ...), "Honolulu" (80.3 81.0 ...), "South Bend" (55.8 54.5 ...)}
 ; store it on a variable `temperatures_map`
 ;;;; map-merger: function that merge two maps
 ;;;; city-reducer: callback that takes `e1` `e2` and updates `e1` with what is in `e2`
-(def temperatures_map (reduce city-reducer rows))
+; (def temperatures_map (reduce city-reducer rows))
 
-; Average function
-(defn avg [p] (double (/ (reduce + p)  (count p))))
-; Use `doseq` to compute the statistics per city
-(println "Statistics temperatures per city")
-(doseq [[city temperatures] temperatures_map]
-					 (println city 
-					 			"\n\tmin\t" (apply min temperatures)
-					 			"\n\tmax\t" (apply max temperatures)
-					 			"\n\tavg\t" (avg temperatures)))
+; ; Average function
+; (defn avg [p] (double (/ (reduce + p)  (count p))))
+; ; Use `doseq` to compute the statistics per city
+; (println "Statistics temperatures per city")
+; (doseq [[city temperatures] temperatures_map]
+; 					 (println city 
+; 					 			"\n\tmin\t" (apply min temperatures)
+; 					 			"\n\tmax\t" (apply max temperatures)
+; 					 			"\n\tavg\t" (avg temperatures)))
 
